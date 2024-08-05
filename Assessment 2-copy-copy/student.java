a39e4b1fc8cd35342981333a63f4ed54e7c6b5ae
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-08-05 13:56:11 +1000

    Start the code
    Cretaing a class named student, adding mark number 1,2,3 also the name and id

---
+++ Assessment 2-copy-copy/student.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class MarksManager here.
+ *
+ * @author Jovin Joseph
+ * @version (a version number or a date)
+ */
+
+import java.io.*; 
+import java.util.*;
+/* this is the class for storing the information about each student*/
+class student 
+    {
+        String name;
+        String id;
+        int markNo1;
+        int markNo2;
+        int markNo3;
+        int total;
+        
+public class MarksManager
+{
