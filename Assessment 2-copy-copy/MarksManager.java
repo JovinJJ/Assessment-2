40296a2dbe3080c216115623459769ba859652b8
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-08-06 12:47:33 +1000

    Defining the class MarksManager
    working on the small errors i make while creating this

--- Assessment 2-copy-copy/MarksManager.java
+++ Assessment 2-copy-copy/MarksManager.java
@@ -27,9 +27,32 @@ class Student
             this.markNo1=markNo1;
             this.markNo2=markNo2;
             this.markNo3=markNo3;
-            this.total=markNo1+markNo2+markNo3; //here we calculate the total marks//
+            this.total=markNo1+markNo2+markNo3; 
+            
+            //here we calculate the total marks//
         }
         
-        
+        @Override
+        public String toString()
+        {
+            return name+"("+id=+")-Marks:["+mark1+","+mark2+","+mark3+"]Total:"+total;
+        }
+    }
+    
+    //now we move on to the main class to manage students marks
 public class MarksManager
 {
+    private List<Student>students=new ArrayList<>();
+    // this is how we read student data from a fie
+    public void readFromFile(String fileName)
+    {
+        try (BufferReader br=new BufferReader(new FileReader(fileName)))
+        {
+            String line;
+            while ((line=br.readLine()) !=null)
+            {
+                
+            }
+        }
+    }
+    
