b4f31217f6f32cb7b0950b22f1187c59a0d31b42
JovinJJ <165971583+JovinJJ@users.noreply.github.com>
2024-08-06 11:09:10 +1000

    edited class student
    spelling mistake

--- Assessment 2-copy-copy/student.java
+++ Assessment 2-copy-copy/student.java
@@ -9,7 +9,7 @@
 import java.io.*; 
 import java.util.*;
 /* this is the class for storing the information about each student*/
-class student 
+class Student 
     {
         String name;
         String id;
@@ -17,6 +17,19 @@ class student
         int markNo2;
         int markNo3;
         int total;
+        
+        // this is a constructor to initialize the student data //
+        
+        public Student(String name, String id, int markNo1, int markNo2, int markNo3)
+        {
+            this.name=name;
+            this.id=id;
+            this.markNo1=markNo1;
+            this.markNo2=markNo2;
+            this.markNo3=markNo3;
+            this.total=markNo1+markNo2+markNo3; //here we calculate the total marks//
+        }
+        
         
 public class MarksManager
 {
